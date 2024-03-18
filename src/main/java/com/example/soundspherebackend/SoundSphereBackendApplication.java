9faa8577ee79850324b90958d5807ec5fce84a48
Enrique <elacave@safareyes.es>
2024-03-18 02:58:48 +0100

    Initial commit

---
+++ src/main/java/com/example/soundspherebackend/SoundSphereBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.soundspherebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoundSphereBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SoundSphereBackendApplication.class, args);
+    }
+
+}
